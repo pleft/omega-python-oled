1c1878432fb6d2819aec0e9ffa0e6e4675aa0b74
Panos L <panos.lefteratos@gmail.com>
2017-11-11 00:59:51 +0200

    fast bait

--- OledLib.py
+++ OledLib.py
@@ -1,5 +1,12 @@
 from OmegaExpansion import oledExp
+from OmegaExpansion import onionI2C
 import time
+
+OLED_EXP_ADDR = 0x3C
+OLED_EXP_REG_DATA = 0x40
+OLED_I2C_MAX_BUFFER = 32
+
+i2c = onionI2C.OnionI2C()
 
 oledExp.driverInit()
 oledExp.setImageColumns()
@@ -22,71 +29,29 @@ def putPixel(x, y):
 	for j in range(rowSize-1, -1, -1):                    
 		byte = (byte << 1) | pagebuffer[(y/rowSize)%rows][x*rowSize+j]
 	compactbuffer[(y/rowSize)%rows][x] = byte
-	return
 
-def drawLine(startX, startY, endX, endY):
-	
-	return
+def putBitmap(x, y, bitmap):
+	height = len(bitmap)
+	width = len(bitmap[0])
+	for i in range(width):
+		for j in range(height):
+			if bitmap[j][i] == 1:
+				putPixel(x+i, y+j)
 
 def blit():
+	""" Blits the framebuffer to screen at maximum fast rate """
 	start = int(round(time.time() * 1000))
-        global pagebuffer
+        global compactbuffer
         page = 0
         while page < rows:
                 count = 0
-                while count < width*rowSize:
-                        byte = 0b00000000
-                        for j in range(rowSize-1, -1, -1):
-                                byte = (byte << 1) | pagebuffer[page][count+j]
-                        oledExp.writeByte(byte)
-                        count += rowSize
+                while count < width:
+			bytes=[compactbuffer[page][count+i] for i in range(OLED_I2C_MAX_BUFFER)]
+			i2c.writeBytes(OLED_EXP_ADDR, OLED_EXP_REG_DATA, bytes)
+                        count += OLED_I2C_MAX_BUFFER
                 page += 1
         #after blitting to screen, clear the pagebuffer
         pagebuffer=[[0 for i in range(width*rowSize)] for j in range(rows)]
+	compactbuffer=[[0 for i in range(width)] for j in range(rows)]
 	end = int(round(time.time() * 1000))
 	print("blit() took: " + str(end-start) )
-        return
-
-def blit2():
-	start = int(round(time.time() * 1000))
-	global pagebuffer
-	page = 0
-	while page < rows:
-		count = 0
-		while count < width*rowSize:
-			byte = 0b00000000
-			for j in range(rowSize-1, -1, -1):
-				byte = (byte << 1) | pagebuffer[page][count+j]
-			if byte > 0b00000000:
-				pixel = count/rowSize
-				oledExp.setCursorByPixel(page, pixel)
-				oledExp.writeByte(byte)
-			count += rowSize
-		page += 1
-	#after blitting to screen, clear the pagebuffer
-	pagebuffer=[[0 for i in range(width*rowSize)] for j in range(rows)]
-	end = int(round(time.time() * 1000))                               
-        print("blit2() took: " + str(end-start) )  
-	return
-
-def blit3():                                  
-        start = int(round(time.time() * 1000))
-        global compactbuffer      
-	global pagebuffer               
-        page = 0                              
-        while page < rows:                  
-                count = 0                                 
-                while count < width:                                  
-                        byte = compactbuffer[page][count]
-                        if byte > 0b00000000:                                 
-                                pixel = count                      
-                                oledExp.setCursorByPixel(page, pixel)
-                                oledExp.writeByte(byte)              
-                        count += 1                                   
-                page += 1                                                  
-        #after blitting to screen, clear the pagebuffer                    
-        pagebuffer=[[0 for i in range(width*rowSize)] for j in range(rows)]
-        compactbuffer=[[0 for i in range(width)] for j in range(rows)]
-	end = int(round(time.time() * 1000))                               
-        print("blit3() took: " + str(end-start) )                          
-        return
