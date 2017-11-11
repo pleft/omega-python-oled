4380c23b2fbc2b011ff562d7c543883fd309ccb0
Panos L <panos.lefteratos@gmail.com>
2017-11-11 14:24:17 +0200

    Demo drawing rectangles

---
+++ RectanglesDemo.py
@@ -0,0 +1,7 @@
+import OledLib
+
+for i in range(10):
+	OledLib.putRectangle(5 + i*2, 5 + i*2, 4*i, 4*i, False)
+	OledLib.putRectangle(OledLib.width - 30 - (10-i)*2, 5 + i*2, 4*(10-i), 4*(10-i), False)
+
+OledLib.blit()
